8853ed63fb18ec4e1cac2251240227b44921f12e
Andrei Shneidar <Andrei.Shneidar@softeq.com>
2016-04-29 12:21:03 +0300

    init

---
+++ EBS.Web/Startup.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+[assembly: OwinStartup(typeof(Elb.WebSocket.Startup))]
+namespace Elb.WebSocket
+{
+
+    // http://owin.org/extensions/owin-WebSocket-Extension-v0.4.0.htm
+    using WebSocketAccept = Action<IDictionary<string, object>, // options
+        Func<IDictionary<string, object>, Task>>; // callback
+    using WebSocketCloseAsync =
+        Func<int /* closeStatus */,
+            string /* closeDescription */,
+            CancellationToken /* cancel */,
+            Task>;
+    using WebSocketReceiveAsync =
+        Func<ArraySegment<byte> /* data */,
+            CancellationToken /* cancel */,
+            Task<Tuple<int /* messageType */,
+                bool /* endOfMessage */,
+                int /* count */>>>;
+    using WebSocketSendAsync =
+        Func<ArraySegment<byte> /* data */,
+            int /* messageType */,
+            bool /* endOfMessage */,
+            CancellationToken /* cancel */,
+            Task>;
+    using WebSocketReceiveResult = Tuple<int, // type
+        bool, // end of message?
+        int>; // count
+    using System.Text;
+    using System.Net;
+    using System.Net.Sockets;
+    public class Startup
+    {
+        private string ip;
+
+        // Run at startup
+        public void Configuration(IAppBuilder app)
+        {
+            ip = GetLocalIPAddress();
+
+            app.Use(UpgradeToWebSockets);
+            app.Run(context =>
+            {
+                string t = DateTime.Now.Millisecond.ToString();
+                return context.Response.WriteAsync(t + " Production OWIN App");
+            });
+        }
+
+        // Run once per request
+        private Task UpgradeToWebSockets(IOwinContext context, Func<Task> next)
+        {
+            WebSocketAccept accept = context.Get<WebSocketAccept>("websocket.Accept");
+            if (accept == null)
+            {
+                // Not a websocket request
+                return next();
+            }
+
+            
+            accept(null, WebSocketEcho);
+
+            return Task.FromResult<object>(null);
+        }
+
+        private async Task WebSocketEcho(IDictionary<string, object> websocketContext)
+        {
+            var sendAsync = (WebSocketSendAsync)websocketContext["websocket.SendAsync"];
+            var receiveAsync = (WebSocketReceiveAsync)websocketContext["websocket.ReceiveAsync"];
+            var closeAsync = (WebSocketCloseAsync)websocketContext["websocket.CloseAsync"];
+            var callCancelled = (CancellationToken)websocketContext["websocket.CallCancelled"];
+
+            byte[] buffer = new byte[1024];
+            WebSocketReceiveResult received = await receiveAsync(new ArraySegment<byte>(buffer), callCancelled);
+
+            object status;
+            while (!websocketContext.TryGetValue("websocket.ClientCloseStatus", out status) || (int)status == 0)
+            {
+                string data = Encoding.UTF8.GetString(buffer);
+                var toSend = Encoding.UTF8.GetBytes(data + " ip=" + ip);
+
+                // Echo anything we receive
+                await sendAsync(new ArraySegment<byte>(toSend, 0, toSend.Length), received.Item1, received.Item2, callCancelled);
+
+                received = await receiveAsync(new ArraySegment<byte>(buffer), callCancelled);
+            }
+
+            await closeAsync((int)websocketContext["websocket.ClientCloseStatus"], (string)websocketContext["websocket.ClientCloseDescription"], callCancelled);
+        }
+
+        public static string GetLocalIPAddress()
+        {
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (var ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    return ip.ToString();
+                }
+            }
+            throw new Exception("Local IP Address Not Found!");
+        }
+    }
+}
